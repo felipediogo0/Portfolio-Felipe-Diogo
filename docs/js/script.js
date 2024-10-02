9fbc3cfaae26838a268c44528135bb261471b4bc
dev.felipediogo <87739239+felipediogo0@users.noreply.github.com>
2024-10-02 17:16:36 -0300

    Destaques em componente separado
    Coloquei os destaques em componentes separados e fiz a requisição via Jquery, para facilitar a manutenção do site. Atenção para a atualização das imagens devido as diferenças nas pastas.

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,12 @@
 $(function(){
     $("#menu-canvas").load("./index.html #canva-menu")
 })
+
+$(function(){
+    $("#highlight-articles").load("./partials/highlight-articles.html #articles-highlight")
+})
+
+
+$(function(){
+    $("#highlight-projects").load("./partials/highlight-projects.html #projects-highlight")
+})
