bd61d9ee4f1a399fefdc7145412bae9a65094a66
dev.felipediogo <87739239+felipediogo0@users.noreply.github.com>
2024-10-04 13:11:22 -0300

    Formulário recarregando na página
    Fiz uma atualização com Javascript para que o formulário Formspree seja enviado sem que a página recarregue e seja direcionada para a página de confirmação do serviço.

--- docs/js/script.js
+++ docs/js/script.js
@@ -10,3 +10,46 @@ $(function(){
 $(function(){
     $("#highlight-projects").load("./partials/highlight-projects.html #projects-highlight")
 })
+
+$(function(){
+    $("#footer-port").load("./index.html #port-footer")
+})
+
+$(function(){
+    $("#form-port").load("./index.html #port-form")
+})
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+  
+    const form = this;
+    const status = document.getElementById('status');
+    const data = new FormData(form); // Captura os dados do formulário
+  
+    fetch(form.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        status.innerHTML = "Mensagem enviada com sucesso!";
+        form.reset(); // Limpa o formulário após o envio
+      } else {
+        response.json().then(data => {
+          if (data.hasOwnProperty('errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+          } else {
+            status.innerHTML = "Ocorreu um erro ao enviar a mensagem.";
+          }
+        });
+      }
+    })
+    .catch(error => {
+      status.innerHTML = "Ocorreu um erro ao enviar a mensagem.";
+    });
+  });
+  
